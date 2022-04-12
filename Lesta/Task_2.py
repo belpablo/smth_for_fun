7bbdba8bd5d4d4f525c06ba75de93e19601df53e
Pavel <belpavl.2.0@gmail.com>
2022-04-12 14:53:29 +0300

    Add files via upload

--- Lesta/Task_2.py
+++ Lesta/Task_2.py
@@ -1,10 +1,10 @@
 class CircularBuffer(object):
 
 
-    def __init__(self, max_size=10):
+    def __init__(self, max_size = 10):
         
-        """ Initialize the CircularBuffer with a max_size if set, otherwise
-            max_size will elementsdefault to 10
+        """ Инициализация цикличного буффера размера max_size.
+            По умолчанию max_size = 10
         """
         
         self.buffer = [None] * max_size
@@ -15,7 +15,7 @@ class CircularBuffer(object):
 
     def __str__(self):
         
-        """ Return a formatted string representation of this CircularBuffer"""
+        """ Визуализация буффера в виде строки элементов посредством метода str"""
         
         items = ['{!r}'.format(item) for item in self.buffer]
         
@@ -24,40 +24,30 @@ class CircularBuffer(object):
 
     def size(self):
         
-        """ Return the size of the CircularBuffer
-            Runtime: O(1) Space: O(1)
-        """
+        """ Размер буфера """
         
         if self.tail >= self.head:
             return self.tail - self.head
         else:
             return self.max_size - self.head - self.tail
 
-
     def is_empty(self):
         
-        """ Return True if the head of the CircularBuffer is equal to the tail,
-            otherwise return False
-            Runtime: O(1) Space: O(1)
-        """
+        """ проверка пустоты буфера """
         
         return self.tail == self.head
 
 
     def is_full(self):
-        """ Return True if the tail of the CircularBuffer is one before the head,
-            otherwise return False
-            Runtime: O(1) Space: O(1)
-        """
+        
+        """ Проверка заполненности буфера """
         
         return self.tail == (self.head-1) % self.max_size
 
 
     def enqueue(self, item):
         
-        """ Insert an item at the back of the CircularBuffer
-            Runtime: O(1) Space: O(1)
-        """
+        """ Вставка элемента в буфер """
         
         if self.is_full():
             raise OverflowError(
@@ -67,20 +57,9 @@ class CircularBuffer(object):
         self.tail = (self.tail + 1) % self.max_size
 
 
-    def front(self):
-        
-        """ Return the item at the front of the CircularBuffer
-            Runtime: O(1) Space: O(1)
-        """
-        
-        return self.buffer[self.head]
-
-
     def dequeue(self):
         
-        """ Return the item at the front of the Circular Buffer and remove it
-            Runtime: O(1) Space: O(1)
-        """
+        """ Вызов голвоного элемента в буфере с последующим его удалением """
         
         if self.is_empty():
             raise IndexError("CircularBuffer is empty, unable to dequeue")
@@ -94,23 +73,17 @@ class CircularBuffer(object):
 
 if __name__ == "__main__":
     
-    cb = CircularBuffer(5)
-    print("Empty: {}".format(cb.is_empty()))
-    print("Full: {}".format(cb.is_full()))
-    print(str(cb))
-    
+    cb = CircularBuffer(5)    
     cb.enqueue("one")
     cb.enqueue("two")
     cb.enqueue("three")
     cb.enqueue("four")
-    
     print(str(cb))
+    
     print(cb.dequeue())
     print(cb.dequeue())
     print(str(cb))
     
     cb.enqueue("five")
     cb.enqueue("six")
-    
     print(str(cb))
-    print("Full: {}".format(cb.is_full()))
