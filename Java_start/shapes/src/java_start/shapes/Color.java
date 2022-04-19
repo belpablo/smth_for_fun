7906d3c486b29456d9c5003d0b2a467ff04e2566
Pavel <belpavl.2.0@gmail.com>
2022-04-19 21:33:08 +0300

    Add files via upload

---
+++ Java_start/shapes/src/java_start/shapes/Color.java
@@ -0,0 +1,9 @@
+package java_start.shapes;
+
+public enum Color {
+    BLACK,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE
+}
---
+++ Java_start/shapes/src/java_start/shapes/Shape.java
@@ -0,0 +1,17 @@
+package java_start.shapes;
+
+public class Shape {
+    private final Color color;
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return Double.NaN;
+    }
+}
