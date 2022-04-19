46a2950f4cd7210ab0474ade3807b6c5226e6be7
Pavel <belpavl.2.0@gmail.com>
2022-04-19 19:52:12 +0300

    Add files via upload

--- Java_start/RobotGame/src/Robot.java
+++ Java_start/RobotGame/src/Robot.java
@@ -1,3 +1,10 @@
+/**
+ * начальное состояние робота такое:
+ * robot.getX() == 0,
+ * robot.getY() == 0,
+ * robot.getDirection() == Direction.UP
+ */
+
 public class Robot {
 
     int x = 0;
@@ -25,7 +32,7 @@ public class Robot {
     }
 
     public void turnLeft() {
-        System.out.println("Поворот против часовой стрелки");
+        //System.out.println("Поворот против часовой стрелки");
         if (direction == Direction.DOWN) {
             this.direction = Direction.RIGHT;
             return;
@@ -48,51 +55,51 @@ public class Robot {
     }
 
     public void turnRight() {
-        System.out.println("поворот по часовой стрелке");
+        //System.out.println("поворот по часовой стрелке");
         if (this.direction == Direction.DOWN) {
-            System.out.println("Вниз -> влево");
+            //System.out.println("Вниз -> влево");
             this.direction = Direction.LEFT;
             return;
         }
 
         if (this.direction == Direction.UP) {
-            System.out.println("Вверх -> вправо");
+            //System.out.println("Вверх -> вправо");
             this.direction = Direction.RIGHT;
             return;
         }
 
         if (this.direction == Direction.LEFT) {
-            System.out.println("Влево -> вверх");
+            //System.out.println("Влево -> вверх");
             this.direction = Direction.UP;
             return;
         }
 
         if (this.direction == Direction.RIGHT) {
-            System.out.println("Вправо -> вниз");
+            //System.out.println("Вправо -> вниз");
             this.direction = Direction.DOWN;
             return;
         }
     }
 
     public void stepForward() {
-        System.out.println("движение");
+        //System.out.println("движение");
         if (direction == Direction.DOWN) {
-            System.out.println("вниз");
+            //System.out.println("вниз");
             this.y--;
         }
 
         if (direction == Direction.UP) {
-            System.out.println("вверх");
+            //System.out.println("вверх");
             this.y++;
         }
 
         if (direction == Direction.LEFT) {
-            System.out.println("налево");
+            //System.out.println("налево");
             this.x--;
         }
 
         if (direction == Direction.RIGHT) {
-            System.out.println("направо");
+            //System.out.println("направо");
             this.x++;
         }
     }
--- Java_start/RobotGame/src/RobotTest.java
+++ Java_start/RobotGame/src/RobotTest.java
@@ -1,6 +1,49 @@
+import java.util.Arrays;
+
 public class RobotTest {
     public static void main(String[] args) {
+
         Robot robot = new Robot();
-        System.out.println(robot.getX());
+
+        moveRobot(robot, 3, 0);
+        System.out.println(Arrays.toString(new int[]{robot.getX(), robot.getY()}));
+        System.out.println(robot.getDirection());
+
+        moveRobot(robot, 0, 3);
+        System.out.println(Arrays.toString(new int[]{robot.getX(), robot.getY()}));
+        System.out.println(robot.getDirection());
+
     }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+
+        while ((toX - robot.getX() != 0) || (toY - robot.getY() != 0)) {
+
+            if (toX - robot.getX() > 0) {
+                while (robot.getDirection() != Robot.Direction.RIGHT) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+
+            } else if (toX - robot.getX() < 0) {
+                while (robot.getDirection() != Robot.Direction.LEFT) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+
+            } else if (toY - robot.getY() > 0) {
+                while (robot.getDirection() != Robot.Direction.UP) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+
+            } else {
+                while (robot.getDirection() != Robot.Direction.DOWN) {
+                    robot.turnLeft();
+                }
+                robot.stepForward();
+            }
+        }
+    }
+
 }
