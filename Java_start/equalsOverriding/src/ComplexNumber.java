e86c06be04b4826fb721cbb297cf6e18a41ecabc
Pavel <belpavl.2.0@gmail.com>
2022-04-19 22:09:32 +0300

    Add files via upload

---
+++ Java_start/equalsOverriding/src/ComplexNumber.java
@@ -0,0 +1,28 @@
+public final class ComplexNumber {
+
+    private final double re;
+    private final double im;
+
+    public ComplexNumber(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double getRe() {
+        return re;
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
+}
