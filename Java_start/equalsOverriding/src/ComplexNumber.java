52955a01e558004bcf5b0d387f48ecf5e31d843d
Pavel <belpavl.2.0@gmail.com>
2022-04-19 23:03:04 +0300

    Add files via upload

--- Java_start/equalsOverriding/src/ComplexNumber.java
+++ Java_start/equalsOverriding/src/ComplexNumber.java
@@ -18,26 +18,39 @@ public final class ComplexNumber {
 
     @Override
     public int hashCode() {
+
         final int prime = 42;
         int result = 1;
-        result = prime * result + (int) this.getRe();
-        result = prime * result + (int) this.getIm();
+
+        long num_re = Double.doubleToLongBits(this.getRe());
+        result = prime * result + (int) (num_re ^ (num_re >>> 32));
+
+        long num_im = Double.doubleToLongBits(this.getIm());
+        result = prime * result + (int) (num_im ^ (num_im >>> 32));
+
         return result;
     }
 
     @Override
     public boolean equals(Object obj) {
+
         if (this == obj)
             return true;
+
         if (obj == null)
             return false;
+
         if (getClass() != obj.getClass())
             return false;
+
         ComplexNumber other = (ComplexNumber) obj;
+
         if (this.getRe() != other.getRe())
             return false;
+
         if (this.getIm() != other.getIm())
             return false;
+
         return true;
     }
 }
