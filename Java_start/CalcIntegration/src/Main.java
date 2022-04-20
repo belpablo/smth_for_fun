2e54dbc357e7f87c4423310fd021d972abc278df
Pavel <belpavl.2.0@gmail.com>
2022-04-20 13:12:51 +0300

    Add files via upload

--- Java_start/CalcIntegration/src/Main.java
+++ Java_start/CalcIntegration/src/Main.java
@@ -8,6 +8,9 @@ import java.util.function.DoubleUnaryOperator;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Тесты:
+
         System.out.println(integrate(x -> 1, 0, 10));//10.0
         System.out.println(integrate(x -> x + 2, 0, 10));//70.0
         System.out.println(integrate(x -> Math.sin(x) / x, 1, 5));//0.603848
@@ -15,12 +18,31 @@ public class Main {
 
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
 
-        double res = 0.0;
-        double eps = 1e-6;
-        int grid_len = (int) (((b - a) / eps) + 1);
+        // Численное интегрирование на динамической сетке (eps = const = 1e-6):
 
-        for (int i = 1; i < grid_len; i++) {
-            res += f.applyAsDouble(a + (i * eps)) * eps;
+        final var eps = 1e-6;
+
+        // Пробуем разные шаги сетки формата eps = 1e-lg_eps, где 0 < lg_eps < 7 :
+
+        int grid_len;
+        double res = f.applyAsDouble(a) * (b - a);
+        double[] res_arr = new double[7];
+        res_arr[0] = res;
+
+        for (int lg_eps = 1; lg_eps < 7; lg_eps ++) {
+
+            grid_len = (int) ((b - a) * Math.pow(10, lg_eps)) + 1;
+
+            res = 0.0;
+            for (int i = 1; i < grid_len; i++) {
+                res += f.applyAsDouble(a + (i * eps)) * eps;
+            }
+
+            res_arr[lg_eps] = res;
+            if (Math.abs(res_arr[lg_eps] - res_arr[lg_eps - 1]) < eps) {
+                System.out.println(lg_eps);
+                break;
+            }
         }
 
         return res;
